e7820ca3c896346ce4b6ad099690d19de76fa0f3
ilypsj <goodperiodt@gmail.com>
2024-05-01 16:12:57 +0900

    1.react(초기설정 완료 및 개념 확인)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import NoName from './NoName';
 
 function App() {
+
+const $h2 = <h2>반가워요~~~</h2>
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <NoName />
+    <div>
+      <div className='App'>
+        <h1>메롱멜옹~</h1>
+        {$h2}
+      </div>
+      <div className='noname'>
+        <input type='text'/>
+          <p>
+            오늘은 5월 1일 수요일 입니다.<br/>
+            오후 3시 30분입니다.
+          </p>
+        
+      </div>
     </div>
+    </>
+    
   );
 }
 
---
+++ src/NoName.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+const NoName = () => {
+  return (
+    <div className='wrap'>
+      <h1>하이하이~~~</h1>
+    </div>
+  )
+}
+
+export default NoName;
